de091bad62785f8d4e6f35bdf0d12ef0e67b0d23
Andrew <lilsneezer87@gmail.com>
2023-01-14 11:37:41 +0400

    Поправил замечания: - добавить enum HealthState для состояния здоровья
    - в классе Animal поле counter сделать static и увеличивать при каждом создании животного
    
    - поправить вывод в toString в классе Animal
    
    - поправить уровень доступа до полей класса Animal до protected и проверить, что используются наследуемые поля в дочерних классах
    
    - поправить метод eat
    
    - поправить метод sleep
    
    - подумать на счет поля sound в классе Animal: добавлять ли, устанавливать значение через метод или конструктор
    
    - поправить метод makeSound
    
    Так же в каждый класс наследник добавил свой счетчик особей

---
+++ src/test/java/part3/lesson19/HealthState.java
@@ -0,0 +1,6 @@
+package part3.lesson19;
+
+public enum HealthState {
+    HEALTHY,
+    UNHEALTHY
+}
--- src/test/java/part3/lesson19/MainLesson19.java
+++ src/test/java/part3/lesson19/MainLesson19.java
@@ -3,26 +3,40 @@ package part3.lesson19;
 public class MainLesson19 {
     public static void main(String[] args) {
 
-        Dog dog = new Dog("Микки", "Корм для собак", 3, "Москва", 1000, "HEALTHY");
-        Horse horse = new Horse("Том", "сено", 15, "лес", 2000, "UNHEALTHY");
-        Cat cat = new Cat("Барсик", "Корм для кошек", 2, "Москва", 3000, "HEALTHY");
-        Vet vet = new Vet();
-
-        //вывод общей инф-ции о каждой особи
+        Dog dog = new Dog("Микки", "Корм для собак", 3, "Москва", HealthState.HEALTHY, "Гав!");
         System.out.println(dog);
+
+        System.out.println("----------------------------------");
+
+        Dog dog1 = new Dog("Микки", "Корм для собак", 3, "Москва", HealthState.HEALTHY, "Гав!");
+
+        System.out.println("----------------------------------");
+
+        Horse horse = new Horse("Том", "сено", 15, "лес", HealthState.UNHEALTHY, "Иго-го!");
         System.out.println(horse);
+
+        System.out.println("----------------------------------");
+
+        Cat cat = new Cat("Барсик", "Корм для кошек", 2, "Москва", HealthState.HEALTHY, "Мяу!");
         System.out.println(cat);
 
+        System.out.println("----------------------------------");
+
+        Cat cat1 = new Cat("Барсик", "Корм для кошек", 2, "Москва", HealthState.HEALTHY, "Мяу!");
+
+        Vet vet = new Vet();
+
         //вызов у каждой особи метода makeSound
-        dog.makeSound("Гав!");
-        horse.makeSound("Иго-го!");
-        cat.makeSound("Мяу!");
+        dog.makeSound();
+        horse.makeSound();
+        cat.makeSound();
 
         //вызовите метод eat у некоторых животных
-        dog.eat(5, 1, "Микки");
+        dog.eat(1);
+        horse.eat(3);
 
         //вызовите метод sleep у некоторых животных
-        cat.sleep("Барсик");
+        cat.sleep();
 
         //создайте ветеринара и отведите каждого животного на прием
         vet.treatAnimal(dog);
