9472c798bdae2c5150091a050abce2a46a01d676
Andrew <lilsneezer87@gmail.com>
2023-03-19 21:21:55 +0300

    Исправлены замечания

--- src/test/java/task2_58/Issue2.java
+++ src/test/java/task2_58/Issue2.java
@@ -4,20 +4,36 @@ package task2_58;
 public class Issue2 {
     public static void main(String[] args) {
         String reg = "[a-z0-9]+";
+        String maxStr = "";
+        int nextIndex = 0;
+        int j;
+
         if (args.length == 0) {
             System.out.println("Аргументы не переданы");
-        } else {
-            String maxStr = args[0];
-            for (int i = 1; i < args.length; i++) {
-                if (args[i].matches(reg)) {
-                    if (args[i].length() > maxStr.split(" ")[0].length()) {
-                        maxStr = args[i];
-                    } else if (args[i].length() == maxStr.split(" ")[0].length()) {
-                        maxStr = maxStr + " " + args[i];
-                    }
+        }
+
+        for (j = 0; j < args.length; j++) {
+            if (args[j].matches(reg) && args[j].equals(args[j].toLowerCase())) {
+                maxStr = args[j];
+                nextIndex = j + 1;
+                break;
+            }
+        }
+
+        for (int i = nextIndex; i < args.length; i++) {
+            if (args[i].matches(reg) && args[i].equals(args[i].toLowerCase())) {
+                if (args[i].length() > maxStr.split(" ")[0].length()) {
+                    maxStr = args[i];
+                } else if (args[i].length() == maxStr.split(" ")[0].length()) {
+                    maxStr = maxStr + " " + args[i];
                 }
             }
-            System.out.println(maxStr);
         }
+
+        if (maxStr.length() == 0) {
+            System.out.println("Переданы невалидные слова");
+        }
+            System.out.println(maxStr);
     }
 }
+
--- src/test/java/task2_58/Issue5.java
+++ src/test/java/task2_58/Issue5.java
@@ -1,21 +1,64 @@
 package task2_58;
 
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Issue5 {
     public static void main(String[] args) {
+        int rowsCount = generateRowsCount();             //генерим кол-во строк
+        int[][] arr = fillArray(rowsCount);              //заполняем массив
 
-        int rowsCount = (int) (Math.random() * (6 - 1)) + 1; //генерим кол-во строк
-        int[][] arr;
-
-        for (int i = 0; i < rowsCount; i++) {
-            int columnCount = (int) (Math.random() * (8 - 1)) + 1; //для каждой строки генерим кол-во столбцов
-            arr = new int[rowsCount][columnCount]; //инициализируем массив
-            System.out.println();
-            for (int j = 0; j < columnCount; j++) { //в цикле заполняем массив рандомными числами
-                arr[i][j] = (int) (Math.random() * (-10 - 9)) + 9;
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println(" || columnCount: " + columnCount); //вывод кол-ва столбцов каждой строки для удобства
+        System.out.println("Raw array:");
+        for (int[] elem: arr) {
+            System.out.println(Arrays.toString(elem));
         }
-        System.out.println("\n" + "rowsCount: " + rowsCount); //вывод кол-ва строк для удобства
+
+        int[][] sortedArr = sortArray(arr);
+        System.out.println("Sort array:");
+        for (int[] elem: sortedArr) {
+            System.out.println(Arrays.toString(elem));
+        }
+    }
+
+    public static int generateRowsCount() {
+        return (int) (Math.random() * (6 - 1)) + 1;
+    }
+
+    public static int generateColumnCount() {
+        return (int) (Math.random() * (8 - 1)) + 1;
+    }
+
+    public static int[][] fillArray(int rows) {                         //заполняем массив и для каждой строки
+        int[][] arr = new int[rows][];                                  //генерим кол-во элементов
+        for (int i = 0; i < rows; i++) {
+            int columnCount = generateColumnCount();
+            arr[i] = new int[columnCount];
+            for (int j = 0; j < columnCount; j++) {
+                arr[i][j] = (int) (Math.random() * (-10 - 9)) + 9;
+            }
+        }
+        return arr;
+    }
+
+    public static int[][] sortArray(int[][] arr) {
+        int[] forChange;
+        int sum;
+        int nextSum;
+        int i;
+        for (int k = 0; k < arr.length; k++) {
+            i = 0;
+            while (i < arr.length - 1) {
+                sum = IntStream.of(arr[i]).sum();
+                nextSum = IntStream.of(arr[i + 1]).sum();
+                if (sum < nextSum) {
+                    forChange = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = forChange;
+                }
+                i++;
+            }
+        }
+        return arr;
     }
 }
