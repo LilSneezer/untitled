116115618be016d7dde9d2c340a96079775cfb67
Andrew <lilsneezer87@gmail.com>
2023-07-10 09:11:06 +0300

    Индивид. задание 10.3(1)
    Добавил Gradle и JUnit
    Изменил задание 2.54(1)
    Сделал тесты для задания 2.54(1)

--- src/test/java/task2_54/Issue1.java
+++ src/test/java/task2_54/Issue1.java
@@ -4,7 +4,11 @@ public class Issue1 {
     public static String str = "Циклом называется многократное выполнение одних и тех же действий.";
     public static int subStrLength;
     public static void main(String[] args) {
-        subStrLength = str.substring(0, str.indexOf(args[0])).replaceAll("\\s", "").length();
-        System.out.println(subStrLength);
+        System.out.println(getSubStrLength(args[0]));
+    }
+
+    public static int getSubStrLength(String latter) {
+        subStrLength = str.substring(0, str.indexOf(latter)).replaceAll("\\s", "").length();
+        return subStrLength;
     }
 }
